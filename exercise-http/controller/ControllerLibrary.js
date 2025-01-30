292c6ed299fde3caf2c97a8c2684a073b149ab4b
Anthony1406 <anthony.hascoet14062003@gmail.com>
2025-01-30 11:57:52 +0100

    Supprimer tous les fichiers de la branche main

--- exercise-http/controller/ControllerLibrary.js
+++
@@ -1,8 +0,0 @@
-const debug = require("debug")("ControllerLibrary");
-
-
-module.exports.getPlageLibPy = async function (req, res) {
-  debug("Sending PlageLib.py");
-  const path = __dirname + "/../lib/Python/plageLib.py";
-  res.download(path);
-};
--- other-http/controller/ControllerLibrary.js
+++
@@ -1,8 +0,0 @@
-const debug = require("debug")("ControllerLibrary");
-//const ModelFile = require("../model/ModelFile");
-
-module.exports.getPlageLibPy = async function (req, res) {
-  debug("Sending PlageLib.py");
-  const path = __dirname + "/../lib/Python/plageLib.py";
-  res.download(path);
-};
